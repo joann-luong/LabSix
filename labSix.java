2641b82565ced555ce2e938e9bc868583b14c3d7
Joann Luong <joann.luong@ufl.edu>
2022-03-15 15:22:12 -0400

    allowing subtraction to occur now

--- labSix.java
+++ labSix.java
@@ -7,14 +7,36 @@ public class labSix {
         double inputOne;
         double inputTwo;
         double result;
+        int input;
+        
+        System.out.print("Do you want to add or subtract? Type 1 to add, or type 2 to subtract")
+        input = scnr.nextInt();
+        if (input == 1){
+            System.out.print("\nEnter the first input: ");
+            inputOne = scnr.nextDouble();
+            System.out.print("\nEnter the second input: ");
+            inputTwo = scnr.nextDouble();
 
-        System.out.print("Enter the first input: ");
-        inputOne = scnr.nextDouble();
-        System.out.print("\nEnter the second input: ");
-        inputTwo = scnr.nextDouble();
+            result = inputOne + inputTwo;
+            System.out.print("\nThe answer is: " + result);
+        }
+        else if (input == 2){
+            System.out.print("\nEnter the first input: ");
+            inputOne = scnr.nextDouble();
+            System.out.print("\nEnter the second input: ");
+            inputTwo = scnr.nextDouble();
 
-        result = inputOne + inputTwo;
-        System.out.print("\nThe answer is: " + result);
+            result = inputOne - inputTwo;
+            System.out.print("\nThe answer is: " + result);
+        }
+
+        //System.out.print("\nEnter the first input: ");
+        //inputOne = scnr.nextDouble();
+        //System.out.print("\nEnter the second input: ");
+        //inputTwo = scnr.nextDouble();
+
+        //result = inputOne + inputTwo;
+        //System.out.print("\nThe answer is: " + result);
         //System.out.println(result);
 
     }
